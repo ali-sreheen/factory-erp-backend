7690c06b88015be6154ce82ecda7fdd3a3f8ef52
Factory Admin <admin@factory-erp.com>
2026-07-21 08:20:53 +0300

    Add Nginx proxy route for /uploads/ in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -107,6 +107,15 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
+
+    # المرفقات والملفات المرفوعة
+    location /uploads/ {
+        proxy_pass http://127.0.0.1:8000/uploads/;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
     
     # توجيه الـ Docs الخاص بالـ API
     location /docs {
