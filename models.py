56f79eee5b81b08c99596dbd13b3beb7d1665828
Factory Admin <admin@factory-erp.com>
2026-07-31 15:24:19 +0300

    Import Boolean type in models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float, Boolean
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from database import Base
